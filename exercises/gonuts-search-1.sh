43a9450593d15a13b7d63a45a14796c953f0304a
Lim Leong Kui <leongkui1976@gmail.com>
2022-04-13 11:03:40 +0800

    change gonuts server

--- exercises/gonuts-search-1.sh
+++ exercises/gonuts-search-1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-/usr/bin/curl -XGET $1':9200/gonuts/_search?pretty' -H 'Content-Type: application/json' -d'
+/usr/bin/curl -XGET 'https://es.leongkui.me/gonuts/_search?pretty' -H 'Content-Type: application/json' -d'
 {
   "query": {
           "term": { "body": "golang" }
